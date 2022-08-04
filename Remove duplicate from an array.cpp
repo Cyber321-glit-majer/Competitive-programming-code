5a043f8aeda1c7243b68e0a45f8589b82db2bd5a
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-08-04 22:43:22 +0530

    Update Remove duplicate from an array.cpp

--- Remove duplicate from an array.cpp
+++ Remove duplicate from an array.cpp
@@ -1,3 +1,5 @@
+// Methond 1   By using hash
+
 
 #include <bits/stdc++.h>
 
@@ -28,6 +30,43 @@ int main()
     for(auto i:v1)
     {
         cout<<v1[i]<<" ";
+    }
+
+    return 0;
+}
+
+
+//Method 2 by using sorting
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> fun(vector<int> &v,int n){
+    
+    vector<int> p;
+    sort(v.begin(),v.end());
+    p.push_back(v[0]);
+    for(int i=1;i<n;i++)
+    {
+        if(v[i]!=v[i-1])
+        {
+            p.push_back(v[i]);
+        }
+       
+    }
+    return p;
+}
+int main()
+{
+    int n=5;
+    vector<int>v={2,2,3,2,5};
+    vector<int>v1;
+   
+    v1=fun(v,n);
+    for(auto const i:v1)
+    {
+        cout<<i<<" ";
     }
 
     return 0;
